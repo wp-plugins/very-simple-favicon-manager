3d49c4456ccebea37eb7939ca9060b4a03bc72a4
Guido07111975 <Guido07111975@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 22:09:02 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/very-simple-favicon-manager/trunk@1220384 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,15 +1,21 @@
 === Very Simple Favicon Manager ===
 Contributors: Guido07111975
-Version: 1.2
+Version: 1.3
 License: GNU General Public License v3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Requires at least: 3.7
 Tested up to: 4.3
 Stable tag: trunk
-Tags: simple, favicon, manager, site, icon, ico, bookmark
+Tags: simple, favicon, manager, site, icon, ico, bookmark, app, ios, android
 
 
 == Changelog == 
+Version 1.3
+- MAJOR UPDATE: now you can set app icons for IOS and Android too
+- updated file readme
+- updated language files
+
+
 Version 1.2
 - updated files vsfm and readme
 - updated language files
@@ -22,9 +28,9 @@ Version 1.0
 
 
 == DESCRIPTION ==
-This is a very simple plugin to add a favicon (site icon) in the address bar of your browser.
+This is a very simple plugin to add an IOS or Android app icon or a favicon in the address bar of your browser.
 
-When no favicon is added, the default WordPress favicon will be used.
+When no favicon or app icon is added, the default WordPress favicon or app icon will be used.
 
 Question? Please take a look at the FAQ section.
 
@@ -44,7 +50,7 @@ Guido
 
 
 == INSTALLATION == 
-After installation upload your favicon (.ico file) in the media library.
+After installation upload your favicon (.ico file or .png file) and/or app icon (.png file) in the media library.
 
 Copy file link.
 
@@ -54,11 +60,17 @@ Paste file link and press save.
 
 
 == Frequently Asked Questions ==
-= What's the best image extension and size for a favicon? =
-Nowadays there are several default favicon sizes, depending on browser and device. I recommend using a square icon with extension .ico and size 16x16px or 32x32px.
+= What are the best image extensions and sizes for a favicon and app icon? =
+Nowadays there are several default favicon and app icon sizes, depending on browser and device. 
 
-= Can I use .png or .jpg file in stead of .ico file? =
-Yes, but the favicon will not be visible in all browsers.
+For desktop and laptop browsers I recommend using a square icon with extension .ico or .png and size 16x16px or 32x32px.
+
+For IOS device I recommend using a square icon with extension .png and size 180x180px.
+
+For Android device I recommend using a square icon with extension .png and size 192x192px.
+
+= Can I use your plugin in combination with the new site icon feature in WordPress dashboard? =
+No, you should not use both.
 
 = Other question or comment? =
 Please open a topic in plugin forum or send me a message via my website.
--- vsfm.php
+++ vsfm.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: Very Simple Favicon Manager
- * Description: This is a very simple plugin to add a favicon in the address bar of your browser. For more info please check readme file.
- * Version: 1.2
+ * Description: This is a very simple plugin to add an IOS or Android app icon or a favicon in the address bar of your browser. For more info please check readme file.
+ * Version: 1.3
  * Author: Guido van der Leest
  * Author URI: http://www.guidovanderleest.nl
  * License: GNU General Public License v3 or later
@@ -21,28 +21,57 @@ add_action('plugins_loaded', 'vsfm_init');
 
 // add the admin options page
 function vsfm_menu_page() {
-    add_options_page( __( 'Favicon Manager', 'favicon' ), __( 'Favicon Manager', 'favicon' ), 'manage_options', 'vsfm', 'vsfm_options_page' );
+	add_options_page( __( 'Favicon Manager', 'favicon' ), __( 'Favicon Manager', 'favicon' ), 'manage_options', 'vsfm', 'vsfm_options_page' );
 }
 add_action( 'admin_menu', 'vsfm_menu_page' );
 
 
 // add the admin settings and such 
 function vsfm_admin_init() {
-    register_setting( 'vsfm-options', 'vsfm-setting', 'sanitize_text_field' );
-    add_settings_section( 'vsfm-section', __( 'How it works', 'favicon' ), 'vsfm_section_callback', 'vsfm' );
-    add_settings_field( 'vsfm-field', __( 'Your Favicon', 'favicon' ), 'vsfm_field_callback', 'vsfm', 'vsfm-section' );
+	add_settings_section( 'vsfm-section', __( 'Favicon (16x16px or 32x32px)', 'favicon' ), 'vsfm_section_callback', 'vsfm' );
+	add_settings_field( 'vsfm-field', __( 'Your Favicon', 'favicon' ), 'vsfm_field_callback', 'vsfm', 'vsfm-section' );
+	register_setting( 'vsfm-options', 'vsfm-setting', 'sanitize_text_field' );
+
+	add_settings_section( 'vsfm-section-ios', __( 'IOS app icon (180x180px)', 'favicon' ), 'vsfm_section_callback_ios', 'vsfm' );
+	add_settings_field( 'vsfm-field-ios', __( 'Your IOS app icon', 'favicon' ), 'vsfm_field_callback_ios', 'vsfm', 'vsfm-section-ios' );
+	register_setting( 'vsfm-options', 'vsfm-setting-ios', 'sanitize_text_field' );
+
+	add_settings_section( 'vsfm-section-android', __( 'Android app icon (192x192px)', 'favicon' ), 'vsfm_section_callback_android', 'vsfm' );
+	add_settings_field( 'vsfm-field-android', __( 'Your Android app icon', 'favicon' ), 'vsfm_field_callback_android', 'vsfm', 'vsfm-section-android' );
+	register_setting( 'vsfm-options', 'vsfm-setting-android', 'sanitize_text_field' );
 }
 add_action( 'admin_init', 'vsfm_admin_init' );
 
 
 function vsfm_section_callback() {
-    echo __( 'Upload your favicon (.ico file) in the media library and copy-paste link here.', 'favicon' ); 
+    echo __( 'Upload your favicon (.ico or .png file) in the media library and copy-paste link here.', 'favicon' ); 
+}
+
+function vsfm_section_callback_ios() {
+    echo __( 'Upload your icon (.png file) in the media library and copy-paste link here.', 'favicon' ); 
 }
 
 
+function vsfm_section_callback_android() {
+	echo __( 'Upload your icon (.png file) in the media library and copy-paste link here.', 'favicon' ); 
+}
+
+
+// add input fields
 function vsfm_field_callback() {
-    $vsfm_setting = esc_url( get_option( 'vsfm-setting' ) );
-    echo "<input type='text' size='60' maxlength='150' name='vsfm-setting' value='$vsfm_setting' />";
+	$vsfm_setting = esc_url( get_option( 'vsfm-setting' ) );
+	echo "<input type='text' size='60' maxlength='150' name='vsfm-setting' value='$vsfm_setting' />";
+
+}
+
+function vsfm_field_callback_ios() {
+	$vsfm_setting_ios = esc_url( get_option( 'vsfm-setting-ios' ) );
+	echo "<input type='text' size='60' maxlength='150' name='vsfm-setting-ios' value='$vsfm_setting_ios' />";
+}
+
+function vsfm_field_callback_android() {
+	$vsfm_setting_android = esc_url( get_option( 'vsfm-setting-android' ) );
+	echo "<input type='text' size='60' maxlength='150' name='vsfm-setting-android' value='$vsfm_setting_android' />";
 }
 
 
@@ -55,26 +84,56 @@ function vsfm_options_page() {
 	<form action="options.php" method="POST">
 	<?php settings_fields( 'vsfm-options' ); ?>
 	<?php do_settings_sections( 'vsfm' ); ?>
-	<?php submit_button(__('Save Favicon', 'favicon')); ?>
+	<?php submit_button(__('Save', 'favicon')); ?>
 	</form>
-	<p><?php _e( 'A favicon is a 16x16px or 32x32px icon with extension .ico which is displayed in the address bar of your browser and in your bookmark list.', 'favicon' ); ?></p>
-	<p><?php _e( 'When no favicon is added, the default WordPress favicon will be used.', 'favicon' ); ?></p>
+	<p><?php _e( 'When no favicon or app icon is added, the default WordPress favicon or app icon will be used.', 'favicon' ); ?></p>
 </div>
 <?php
 }
 
 
 // include favicon in header 
-function vsfm_display_icon() {
-	$vsfm_custom_icon = esc_url( get_option( 'vsfm-setting' ) );
-	$vsfm_default_icon = plugins_url( 'images/favicon.ico', __FILE__ ); 
+function vsfm_display_favicon() {
+	$vsfm_custom_favicon = esc_url( get_option( 'vsfm-setting' ) );
+	$vsfm_default_favicon = plugins_url( 'images/favicon.png', __FILE__ ); 
 
-	if (empty($vsfm_custom_icon)) {
-		echo '<link rel="shortcut icon" href="'.$vsfm_default_icon.'" />'."\n";
+	if (empty($vsfm_custom_favicon)) {
+		echo '<link rel="icon" href="'.$vsfm_default_favicon.'" />'."\n";
 	}
 	else {
-		echo '<link rel="shortcut icon" href="'.$vsfm_custom_icon.'" />'."\n";
+		echo '<link rel="icon" href="'.$vsfm_custom_favicon.'" />'."\n";
 	}
 }
-add_action( 'wp_head', 'vsfm_display_icon' );
+add_action( 'wp_head', 'vsfm_display_favicon' );
+
+
+// include IOS app icon in header 
+function vsfm_display_icon_ios() {
+	$vsfm_custom_icon_ios = esc_url( get_option( 'vsfm-setting-ios' ) );
+	$vsfm_default_icon_ios = plugins_url( 'images/wp-ios.png', __FILE__ ); 
+
+	if (empty($vsfm_custom_icon_ios)) {
+		echo '<link rel="apple-touch-icon" href="'.$vsfm_default_icon_ios.'" sizes="180x180" />'."\n";
+	}
+	else {
+		echo '<link rel="apple-touch-icon" href="'.$vsfm_custom_icon_ios.'" sizes="180x180"/>'."\n";
+	}
+}
+add_action( 'wp_head', 'vsfm_display_icon_ios' );
+
+
+// include Android app icon in header 
+function vsfm_display_icon_android() {
+	$vsfm_custom_icon_android = esc_url( get_option( 'vsfm-setting-android' ) );
+	$vsfm_default_icon_android = plugins_url( 'images/wp-android.png', __FILE__ ); 
+
+	if (empty($vsfm_custom_icon_android)) {
+		echo '<link rel="icon" href="'.$vsfm_default_icon_android.'" sizes="192x192" />'."\n";
+	}
+	else {
+		echo '<link rel="icon" href="'.$vsfm_custom_icon_android.'" sizes="192x192" />'."\n";
+	}
+}
+add_action( 'wp_head', 'vsfm_display_icon_android' );
+
 ?>
