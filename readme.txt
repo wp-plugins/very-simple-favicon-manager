e373ca9dc632880b153edc626f04988df5fdf541
Guido07111975 <Guido07111975@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-01 19:10:53 +0000

    First version
    git-svn-id: https://plugins.svn.wordpress.org/very-simple-favicon-manager/trunk@1235679 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,17 +1,22 @@
 === Very Simple Favicon Manager ===
 Contributors: Guido07111975
-Version: 1.7
+Version: 1.8
 License: GNU General Public License v3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Requires at least: 3.7
 Tested up to: 4.3
 Stable tag: trunk
-Tags: simple, favicon, manager, site, icon, ico, bookmark, app, tile, ios, android, windows
+Tags: simple, favicon, manager, site, icon, ico, bookmark, apple, touch, tile, ios, android, windows
 
 
 == Changelog == 
+Version 1.8
+- removed the Android en Windows input fields
+- added new default multi-size ico
+- updated FAQ
+
 Version 1.7
-- in next update I will remove the Windows app icon again, because Windows uses too many different sizes.
+- in next update I will remove the Windows site icon again, because Windows uses too many different sizes.
 - and because many Android devices use the same icon as IOS devices, I will combine the two in next update.
 - next update first week of september so untill then you can clear the Android and Windows input fields.
 
@@ -19,7 +24,7 @@ Version 1.6
 - please ignore this version
 
 Version 1.5
-- now you can set app icon for windows too
+- now you can set site icon for windows too
 - added new default icons
 - updated language files
 
@@ -28,7 +33,7 @@ Version 1.4
 - updated language files
 
 Version 1.3
-- MAJOR UPDATE: now you can set app icons for IOS and Android too
+- MAJOR UPDATE: now you can set site icons for IOS and Android too
 - updated file readme
 - updated language files
 
@@ -45,16 +50,13 @@ Version 1.0
 
 
 == DESCRIPTION ==
-This is a very simple plugin to add an IOS, Android or Windows app icon or a favicon in the address bar of your browser.
+This is a very simple plugin to add an IOS site icon or a favicon in the address bar of your browser.
 
-When no favicon or app icon is added, a default favicon or app icon will be used.
+When no favicon or site icon is added, a default favicon or site icon will be used.
+
+Because nowadays there are multiple default site icon sizes not all devices are covered. 
 
 Question? Please take a look at the FAQ section.
-
-In next update I will remove the Windows app icon again, because Windows uses too many different sizes. 
-And because many Android devices use the same icon as IOS devices, I will combine the two in next update. 
-Next update first week of september so untill then you can clear the Android and Windows input fields.
-
 
 
 = Translation =
@@ -72,7 +74,7 @@ Guido
 
 
 == INSTALLATION == 
-After installation upload your favicon (.ico file or .png file) and/or your IOS, Android or Windows app icon (.png file) in the media library.
+After installation upload your favicon (.ico file) and/or your IOS site icon (.png file) in the media library.
 
 Copy file link.
 
@@ -82,16 +84,25 @@ Paste file link and press save.
 
 
 == Frequently Asked Questions ==
-= What are the best image extensions and sizes for a favicon and app icon? =
-Nowadays there are several default favicon and app icon sizes, depending on browser and device. 
+= What are the best image extensions and sizes for a favicon and site icon? =
+For desktop and laptop browsers I recommend using a square multi-size .ico file (called favicon) and with sizes 16x16px, 32x32px and 48x48px. The minimum is a single 16x16px favicon.
 
-For desktop and laptop browsers I recommend using a square icon with extension .ico or .png and size 16x16px or 32x32px.
+Nowadays there are multiple default site icon sizes, depending on device. 
 
 For IOS device I recommend using a square icon with extension .png and size 180x180px.
 
-For Android device I recommend using a square icon with extension .png and size 192x192px.
+Android device often uses the IOS site icon.
 
-For Windows device I recommend using a square icon with extension .png and size 270x270px.
+For Windows (mobile) device it's not clear to me.
+
+= What's a multi-size .ico file? =
+An .ico file can contain multiple favicons, so it can contain a 16x16px, 32x32px and 48x48px favicon.
+
+= Can I use a .png in stead of .ico favicon? =
+Normally it's possible (although .png is less supported) but this plugin only supports the .ico favicon.
+
+= What happens when I leave 1 or both input fields blank? =
+When no favicon or site icon link is added, a default favicon or site icon will be used.
 
 = Can I use your plugin in combination with the new site icon feature in WordPress dashboard? =
 No, you should not use both.
--- vsfm.php
+++ vsfm.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: Very Simple Favicon Manager
- * Description: This is a very simple plugin to add an IOS, Android or Windows app icon or a favicon in the address bar of your browser. For more info please check readme file.
- * Version: 1.7
+ * Description: This is a very simple plugin to add an IOS site icon or a favicon in the address bar of your browser. For more info please check readme file.
+ * Version: 1.8
  * Author: Guido van der Leest
  * Author URI: http://www.guidovanderleest.nl
  * License: GNU General Public License v3 or later
@@ -28,27 +28,19 @@ add_action( 'admin_menu', 'vsfm_menu_page' );
 
 // add the admin settings and such 
 function vsfm_admin_init() {
-	add_settings_section( 'vsfm-section', __( 'Favicon (16x16px or 32x32px)', 'favicon' ), 'vsfm_section_callback', 'vsfm' );
+	add_settings_section( 'vsfm-section', __( 'Favicon (16x16px or multi-size)', 'favicon' ), 'vsfm_section_callback', 'vsfm' );
 	add_settings_field( 'vsfm-field', __( 'Your Favicon', 'favicon' ), 'vsfm_field_callback', 'vsfm', 'vsfm-section' );
 	register_setting( 'vsfm-options', 'vsfm-setting', 'sanitize_text_field' );
 
-	add_settings_section( 'vsfm-section-ios', __( 'IOS app icon (180x180px)', 'favicon' ), 'vsfm_section_callback_ios', 'vsfm' );
-	add_settings_field( 'vsfm-field-ios', __( 'Your IOS app icon', 'favicon' ), 'vsfm_field_callback_ios', 'vsfm', 'vsfm-section-ios' );
+	add_settings_section( 'vsfm-section-ios', __( 'IOS site icon (180x180px)', 'favicon' ), 'vsfm_section_callback_ios', 'vsfm' );
+	add_settings_field( 'vsfm-field-ios', __( 'Your IOS site icon', 'favicon' ), 'vsfm_field_callback_ios', 'vsfm', 'vsfm-section-ios' );
 	register_setting( 'vsfm-options', 'vsfm-setting-ios', 'sanitize_text_field' );
-
-	add_settings_section( 'vsfm-section-android', __( 'Android app icon (192x192px)', 'favicon' ), 'vsfm_section_callback_android', 'vsfm' );
-	add_settings_field( 'vsfm-field-android', __( 'Your Android app icon', 'favicon' ), 'vsfm_field_callback_android', 'vsfm', 'vsfm-section-android' );
-	register_setting( 'vsfm-options', 'vsfm-setting-android', 'sanitize_text_field' );
-
-	add_settings_section( 'vsfm-section-windows', __( 'Windows app icon (270x270px)', 'favicon' ), 'vsfm_section_callback_windows', 'vsfm' );
-	add_settings_field( 'vsfm-field-windows', __( 'Your Windows app icon', 'favicon' ), 'vsfm_field_callback_windows', 'vsfm', 'vsfm-section-windows' );
-	register_setting( 'vsfm-options', 'vsfm-setting-windows', 'sanitize_text_field' );
 }
 add_action( 'admin_init', 'vsfm_admin_init' );
 
 
 function vsfm_section_callback() {
-    echo __( 'Upload your favicon (.ico or .png file) in the media library and copy-paste link here.', 'favicon' ); 
+    echo __( 'Upload your favicon (.ico file) in the media library and copy-paste link here.', 'favicon' ); 
 }
 
 function vsfm_section_callback_ios() {
@@ -56,35 +48,15 @@ function vsfm_section_callback_ios() {
 }
 
 
-function vsfm_section_callback_android() {
-	echo __( 'Upload your icon (.png file) in the media library and copy-paste link here.', 'favicon' ); 
-}
-
-function vsfm_section_callback_windows() {
-	echo __( 'Upload your icon (.png file) in the media library and copy-paste link here.', 'favicon' ); 
-}
-
-
 // add input fields
 function vsfm_field_callback() {
 	$vsfm_setting = esc_url( get_option( 'vsfm-setting' ) );
 	echo "<input type='text' size='60' maxlength='150' name='vsfm-setting' value='$vsfm_setting' />";
-
 }
 
 function vsfm_field_callback_ios() {
 	$vsfm_setting_ios = esc_url( get_option( 'vsfm-setting-ios' ) );
 	echo "<input type='text' size='60' maxlength='150' name='vsfm-setting-ios' value='$vsfm_setting_ios' />";
-}
-
-function vsfm_field_callback_android() {
-	$vsfm_setting_android = esc_url( get_option( 'vsfm-setting-android' ) );
-	echo "<input type='text' size='60' maxlength='150' name='vsfm-setting-android' value='$vsfm_setting_android' />";
-}
-
-function vsfm_field_callback_windows() {
-	$vsfm_setting_windows = esc_url( get_option( 'vsfm-setting-windows' ) );
-	echo "<input type='text' size='60' maxlength='150' name='vsfm-setting-windows' value='$vsfm_setting_windows' />";
 }
 
 
@@ -94,17 +66,17 @@ function vsfm_options_page() {
 <div class="wrap"> 
 	<div id="icon-plugins" class="icon32"></div> 
 	<h1><?php _e( 'Very Simple Favicon Manager', 'favicon' ); ?></h1> 
-<hr>
-<h3>In next update I will remove the Windows app icon again, because Windows uses too many different sizes.</h3>
-<h3>And because many Android devices use the same icon as IOS devices, I will combine the two in next update.</h3>
-<h3>Next update first week of september so untill then you can clear the Android and Windows input fields.</h3>
-<hr>
+	<hr>
 	<form action="options.php" method="POST">
 	<?php settings_fields( 'vsfm-options' ); ?>
 	<?php do_settings_sections( 'vsfm' ); ?>
 	<?php submit_button(__('Save', 'favicon')); ?>
 	</form>
-	<p><?php _e( 'When no favicon or app icon is added, a default favicon or app icon will be used.', 'favicon' ); ?></p>
+	<hr>
+	<p><?php _e( 'When no favicon or site icon link is added, a default favicon or site icon will be used.', 'favicon' ); ?></p>
+	<p><?php _e( 'For more info please check this page: ', 'favicon' ); ?><a href="https://wordpress.org/plugins/very-simple-favicon-manager/faq" target="_blank">VSFM FAQ</a></p>
+
+
 </div>
 <?php
 }
@@ -113,60 +85,30 @@ function vsfm_options_page() {
 // include favicon in header 
 function vsfm_display_favicon() {
 	$vsfm_custom_favicon = esc_url( get_option( 'vsfm-setting' ) );
-	$vsfm_default_favicon = plugins_url( 'images/favicon.png', __FILE__ ); 
+	$vsfm_default_favicon = plugins_url( 'images/favicon.ico', __FILE__ ); 
 
 	if (empty($vsfm_custom_favicon)) {
-		echo '<link rel="icon" href="'.$vsfm_default_favicon.'" />'."\n";
+		echo '<link rel="shortcut icon" href="'.$vsfm_default_favicon.'" />'."\n";
 	}
 	else {
-		echo '<link rel="icon" href="'.$vsfm_custom_favicon.'" />'."\n";
+		echo '<link rel="shortcut icon" href="'.$vsfm_custom_favicon.'" />'."\n";
 	}
 }
 add_action( 'wp_head', 'vsfm_display_favicon' );
 
 
-// include IOS app icon in header 
+// include IOS site icon in header 
 function vsfm_display_icon_ios() {
 	$vsfm_custom_icon_ios = esc_url( get_option( 'vsfm-setting-ios' ) );
-	$vsfm_default_icon_ios = plugins_url( 'images/icon-ios.png', __FILE__ ); 
+	$vsfm_default_icon_ios = plugins_url( 'images/apple-touch-icon.png', __FILE__ ); 
 
 	if (empty($vsfm_custom_icon_ios)) {
-		echo '<link rel="apple-touch-icon" href="'.$vsfm_default_icon_ios.'" sizes="180x180" />'."\n";
+		echo '<link rel="apple-touch-icon" href="'.$vsfm_default_icon_ios.'" />'."\n";
 	}
 	else {
-		echo '<link rel="apple-touch-icon" href="'.$vsfm_custom_icon_ios.'" sizes="180x180"/>'."\n";
+		echo '<link rel="apple-touch-icon" href="'.$vsfm_custom_icon_ios.'" />'."\n";
 	}
 }
 add_action( 'wp_head', 'vsfm_display_icon_ios' );
-
-
-// include Android app icon in header 
-function vsfm_display_icon_android() {
-	$vsfm_custom_icon_android = esc_url( get_option( 'vsfm-setting-android' ) );
-	$vsfm_default_icon_android = plugins_url( 'images/icon-android.png', __FILE__ ); 
-
-	if (empty($vsfm_custom_icon_android)) {
-		echo '<link rel="icon" href="'.$vsfm_default_icon_android.'" sizes="192x192" />'."\n";
-	}
-	else {
-		echo '<link rel="icon" href="'.$vsfm_custom_icon_android.'" sizes="192x192" />'."\n";
-	}
-}
-add_action( 'wp_head', 'vsfm_display_icon_android' );
-
-
-// include Windows app icon in header 
-function vsfm_display_icon_windows() {
-	$vsfm_custom_icon_windows = esc_url( get_option( 'vsfm-setting-windows' ) );
-	$vsfm_default_icon_windows = plugins_url( 'images/icon-windows.png', __FILE__ ); 
-
-	if (empty($vsfm_custom_icon_windows)) {
-		echo '<meta name="msapplication-TileImage" content="href="'.$vsfm_default_icon_windows.'">'."\n";
-	}
-	else {
-		echo '<meta name="msapplication-TileImage" content="href="'.$vsfm_custom_icon_windows.'">'."\n";
-	}
-}
-add_action( 'wp_head', 'vsfm_display_icon_windows' );
 
 ?>
